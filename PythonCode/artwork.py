ca145d56d7bfc1db4da0f527bbc07e80e10bc92c
najlakhalid <160962558+najlakhalid@users.noreply.github.com>
2024-03-29 04:36:32 +0400

    Update artwork.py

--- PythonCode/artwork.py
+++ PythonCode/artwork.py
@@ -1,18 +1,20 @@
 from PythonCode.entity import Entity, validate_date
 
+# Definition of the Artwork class, inheriting from the Entity class
 class Artwork(Entity):
-     #Create a Instance or Dummy data in it Dynamically
+      # List to store all instances of artworks
     artworks = []
 
-    def __init__(self, title, artist, date_of_creation, historical_significance, exhibition):
+    def __init__(self, title, artist, date_of_creation, historical_significance, exhibition):  # Constructor method to initialize an Artwork object
         self.id = self.generate_id()
         self.title = title
         self.artist = artist
         self.date_of_creation = date_of_creation
         self.historical_significance = historical_significance
         self.exhibition = exhibition
-        Artwork.artworks.append(self)
-    
+        Artwork.artworks.append(self)  # Add the new artwork instance to the list of artworks
+
+      # Class method to display information about all artworks
     @classmethod
     def display_artworks(cls):
         for artwork in cls.artworks:
@@ -22,5 +24,5 @@ class Artwork(Entity):
             print(f"Date of Creation: {artwork.date_of_creation}")
             print(f"Historical Significance: {artwork.historical_significance}")
             print(f"Exhibition: {artwork.exhibition.name}")
-            print("-" * 20)
+            print("-" * 20)  # Printing a separator line
             
