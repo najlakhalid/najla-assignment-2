3507ecaeba2f3a75e07975bb53ed456c25bee4a6
najlakhalid <160962558+najlakhalid@users.noreply.github.com>
2024-03-29 04:55:48 +0400

    Update entity.py

--- PythonCode/entity.py
+++ PythonCode/entity.py
@@ -1,19 +1,20 @@
-import datetime
-#Incremented integerId Id
+import datetime # Importing the datetime module to work with dates and times
+#Incremented integer Id for entities 
 class Entity:
-    last_id = 0
+    last_id = 0  # Initializing the last_id attribute to track the last assigned ID
 
-    def __init__(self):
-        self.id = self.generate_id()
+    def __init__(self):   # Constructor method to initialize an Entity object
+        self.id = self.generate_id()   # Generating a unique ID for each Entity instance
 
+      # Class method to generate a unique ID for each Entity instance
     @classmethod
     def generate_id(cls):
         cls.last_id += 1
         return cls.last_id
- #Date Time validator 
+ #Date Time validator function
 def validate_date(date_string):
     try:
-        datetime.datetime.strptime(date_string, "%Y-%m-%d")
-        return True
+        datetime.datetime.strptime(date_string, "%Y-%m-%d")  # It converts the string into a datetime object representing the parsed date
+        return True  #returns True if the date string is successfully parsed into a datetime object,
     except ValueError:
-        return False
+        return False  # indicating that the date string is not in the correct format and does not represent a valid date.
